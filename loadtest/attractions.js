5d04274a39e21ec306c65de2a13ea5a68ff0606d
kty <kty8600@naver.com>
2025-12-30 15:48:01 +0900

    index 테스트 완료

--- loadtest/attractions.js
+++ loadtest/attractions.js
@@ -1,10 +1,10 @@
 import http from "k6/http";
 
 export const options = {
-  vus: 800, // Virtual Users
+  vus: 200, // Virtual Users
   duration: "2m", // Test duration
 };
 
 export default function () {
-  http.get(`${__ENV.BASE_URL}/api/attractions?contentTypeId=12&page=0&size=20`); // 관광지인 데이터
+  http.get(`${__ENV.BASE_URL}/api/attractions?contentTypeId=12&page=1000&size=20`); // 관광지인 데이터
 }
